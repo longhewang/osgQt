efd0c82996d0a8e8d56572f843aff01ad9a9afa6
Longhe Wang <longhe@longhe.wang>
2023-06-27 19:39:58 +0800

    qt6 support

--- src/osgQOpenGL/OSGRenderer.cpp
+++ src/osgQOpenGL/OSGRenderer.cpp
@@ -257,7 +257,7 @@ void OSGRenderer::mousePressEvent(QMouseEvent* event)
         button = 1;
         break;
 
-    case Qt::MidButton:
+    case Qt::MiddleButton:
         button = 2;
         break;
 
@@ -275,8 +275,8 @@ void OSGRenderer::mousePressEvent(QMouseEvent* event)
     }
 
     setKeyboardModifiers(event);
-    m_osgWinEmb->getEventQueue()->mouseButtonPress(event->x() * m_windowScale,
-                                                   event->y() * m_windowScale, button);
+    m_osgWinEmb->getEventQueue()->mouseButtonPress(event->position().x() * m_windowScale,
+                                                   event->position().y() * m_windowScale, button);
 }
 
 void OSGRenderer::mouseReleaseEvent(QMouseEvent* event)
@@ -289,7 +289,7 @@ void OSGRenderer::mouseReleaseEvent(QMouseEvent* event)
         button = 1;
         break;
 
-    case Qt::MidButton:
+    case Qt::MiddleButton:
         button = 2;
         break;
 
@@ -307,8 +307,8 @@ void OSGRenderer::mouseReleaseEvent(QMouseEvent* event)
     }
 
     setKeyboardModifiers(event);
-    m_osgWinEmb->getEventQueue()->mouseButtonRelease(event->x() * m_windowScale,
-                                                     event->y() * m_windowScale, button);
+    m_osgWinEmb->getEventQueue()->mouseButtonRelease(event->position().x() * m_windowScale,
+                                                     event->position().y() * m_windowScale, button);
 }
 
 void OSGRenderer::mouseDoubleClickEvent(QMouseEvent* event)
@@ -321,7 +321,7 @@ void OSGRenderer::mouseDoubleClickEvent(QMouseEvent* event)
         button = 1;
         break;
 
-    case Qt::MidButton:
+    case Qt::MiddleButton:
         button = 2;
         break;
 
@@ -339,27 +339,27 @@ void OSGRenderer::mouseDoubleClickEvent(QMouseEvent* event)
     }
 
     setKeyboardModifiers(event);
-    m_osgWinEmb->getEventQueue()->mouseDoubleButtonPress(event->x() * m_windowScale,
-                                                         event->y() * m_windowScale, button);
+    m_osgWinEmb->getEventQueue()->mouseDoubleButtonPress(event->position().x() * m_windowScale,
+                                                         event->position().y() * m_windowScale, button);
 }
 
 void OSGRenderer::mouseMoveEvent(QMouseEvent* event)
 {
     setKeyboardModifiers(event);
-    m_osgWinEmb->getEventQueue()->mouseMotion(event->x() * m_windowScale,
-                                              event->y() * m_windowScale);
+    m_osgWinEmb->getEventQueue()->mouseMotion(event->position().x() * m_windowScale,
+                                              event->position().y() * m_windowScale);
 }
 
 void OSGRenderer::wheelEvent(QWheelEvent* event)
 {
     setKeyboardModifiers(event);
-    m_osgWinEmb->getEventQueue()->mouseMotion(event->x() * m_windowScale,
-                                              event->y() * m_windowScale);
+    m_osgWinEmb->getEventQueue()->mouseMotion(event->position().x() * m_windowScale,
+                                              event->position().y() * m_windowScale);
     m_osgWinEmb->getEventQueue()->mouseScroll(
-        event->orientation() == Qt::Vertical ?
-        (event->delta() > 0 ? osgGA::GUIEventAdapter::SCROLL_UP :
+        event->angleDelta().y() != 0 ?
+        (event->angleDelta().y() > 0 ? osgGA::GUIEventAdapter::SCROLL_UP :
          osgGA::GUIEventAdapter::SCROLL_DOWN) :
-        (event->delta() > 0 ? osgGA::GUIEventAdapter::SCROLL_LEFT :
+        (event->angleDelta().x() > 0 ? osgGA::GUIEventAdapter::SCROLL_LEFT :
          osgGA::GUIEventAdapter::SCROLL_RIGHT));
 }
 
